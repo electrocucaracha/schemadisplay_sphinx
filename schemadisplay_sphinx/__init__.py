f8f74af974b48ec2b1e8f9dc1285a82e1dd27336
Victor Morales <victor.morales@intel.com>
2016-02-21 19:44:48 -0600

    Add flake8 validation
    It was created the process to validate the syntax of the source code.
    This is the initial step to ensure the quality of the code.

--- schemadisplay_sphinx/__init__.py
+++ schemadisplay_sphinx/__init__.py
@@ -16,14 +16,18 @@ import os
 from sets import Set
 import sys
 
-from sqlalchemy_schemadisplay import create_uml_graph
 from sqlalchemy.orm import class_mapper
+from sqlalchemy_schemadisplay import create_uml_graph
+
 
 def builder_inited_handler(app):
 
     def is_valid_folder(folder, skip_folders):
+        if len(folder) == 0:
+            return False
         for skip_folder in skip_folders:
-             if skip_folder in folder: return False
+            if skip_folder in folder:
+                return False
         return True
 
     skip_folders = ""
@@ -31,28 +35,32 @@ def builder_inited_handler(app):
         skip_folders = app.config.model_skip_folders.split(",")
 
     str_model_cls = app.config.model_class_name
-    if not str_model_cls: return
+    if not str_model_cls:
+        return
     __import__(str_model_cls[:str_model_cls.rfind(".")])
-    model_class = getattr(sys.modules[str_model_cls[:str_model_cls.rfind(".")]],
-                          str_model_cls[str_model_cls.rfind(".")+1:])
+    obj = sys.modules[str_model_cls[:str_model_cls.rfind(".")]]
+    name = str_model_cls[str_model_cls.rfind(".")+1:]
+    model_class = getattr(obj, name)
 
     app.info("Importing modules")
     cwd = os.getcwd()
     modules = Set()
     for root, directories, filenames in os.walk(cwd):
         for filename in filenames:
-             folder = str(root)
-             if not is_valid_folder(folder, skip_folders): continue
-             if filename.endswith(".py") and not filename.startswith("__"):
-                 name = filename[:-2]
-                 module =  "%s.%s" % (folder.replace(cwd, '').replace("/", "."), name)
-                 modules.add(module[1:-1])
+            folder = str(root)
+            relative_folder = folder.replace(cwd, '')
+            if not is_valid_folder(relative_folder, skip_folders):
+                continue
+            if filename.endswith(".py") and not filename.startswith("__"):
+                name = filename[:-3]
+                module = "%s.%s" % (relative_folder.replace("/", "."), name)
+                modules.add(module[1:])
 
     for module in modules:
         try:
-             __import__(module)
-        except:
-             pass
+            __import__(module)
+        except Exception:
+            pass
 
     app.info("Getting mappers")
     mappers = []
@@ -60,15 +68,15 @@ def builder_inited_handler(app):
         for attr in cls.__dict__.keys():
             try:
                 mappers.append(class_mapper(cls))
-            except:
+            except Exception:
                 pass
 
     app.info("Creating the db schema")
     graph = create_uml_graph(mappers,
-        show_operations=False,
-        show_multiplicity_one=False
-    )
+                             show_operations=False,
+                             show_multiplicity_one=False)
     graph.write_png(app.config.model_schema_filename)
+
 
 def setup(app):
     app.add_config_value('model_class_name', '', 'html')
